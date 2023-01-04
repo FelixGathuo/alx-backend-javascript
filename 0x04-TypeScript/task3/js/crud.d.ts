cfe7ba569c818ef1c4eb407fe1b4c12ec925a62e
FelixGathuo <40053042+FelixGathuo@users.noreply.github.com>
2023-01-04 20:24:57 +0300

    crud t

--- 0x04-TypeScript/task3/js/crud.d.ts
+++ 0x04-TypeScript/task3/js/crud.d.ts
@@ -1,7 +1,5 @@
 import { RowID, RowElement } from './interface';
 
-const row: RowElement = {
-  firstName: 'John',
-  lastName: 'Doe',
-  age: 30
-};
+declare function insertRow(row: RowElement): RowID;
+declare function updateRow(rowID: RowID, row: RowElement): void;
+declare function deleteRow(rowID: RowID): void;
