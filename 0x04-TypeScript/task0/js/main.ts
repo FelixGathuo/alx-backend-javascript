37735a0d38502c0de36ce26ff9805db2a4716ab9
FelixGathuo <40053042+FelixGathuo@users.noreply.github.com>
2023-01-04 19:40:52 +0300

    Update main.ts

--- 0x04-TypeScript/task0/js/main.ts
+++ 0x04-TypeScript/task0/js/main.ts
@@ -5,3 +5,35 @@ age: number;
 location: string;
 }
 
+let student1: Student = {
+firstName: "Felix",
+lastName: "G",
+age: 25,
+location: "Naivasha"
+}
+
+let student2: Student = {
+firstName: "Mercy",
+lastName: "Atieno",
+age: 22,
+location: "Kinangop"
+}
+
+let studentsList = [student1, student2];
+
+let table = document.createElement("table");
+
+for (let student of studentsList) {
+let row = document.createElement("tr");
+let firstName = document.createElement("td");
+let location = document.createElement("td");
+
+firstName.innerHTML = student.firstName;
+location.innerHTML = student.location;
+
+row.appendChild(firstName);
+row.appendChild(location);
+table.appendChild(row);
+}
+
+document.body.appendChild(table);
